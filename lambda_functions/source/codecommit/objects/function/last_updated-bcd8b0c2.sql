461dfb152319f5f4496c3ba38796a94f4a9ad290
Mike Olivas <47544147+molivasdat@users.noreply.github.com>
2022-03-09 10:41:43 -0600

    Delete last-updated, not needed in docs

--- lambda_functions/source/codecommit/objects/function/last_updated-bcd8b0c2.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION public.last_updated()
- RETURNS trigger
- LANGUAGE plpgsql
-AS $function$
-BEGIN
-    NEW.last_update = CURRENT_TIMESTAMP;
-    RETURN NEW;
-END $function$
