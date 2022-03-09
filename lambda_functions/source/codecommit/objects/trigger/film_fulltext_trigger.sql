6681d8a1491073fcff13645eacc7dc522a1ba6ff
Mike Olivas <47544147+molivasdat@users.noreply.github.com>
2022-03-09 10:43:35 -0600

    Delete film-fulltext-trigger, not needed in docs

--- lambda_functions/source/codecommit/objects/trigger/film_fulltext_trigger.sql
+++
@@ -1,1 +0,0 @@
-CREATE TRIGGER film_fulltext_trigger BEFORE INSERT OR UPDATE ON public.film FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('fulltext', 'pg_catalog.english', 'title', 'description')
